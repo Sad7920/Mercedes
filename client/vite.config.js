f46996b1c3b46f7bee2376dbdfc311b6e2342d53
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2023-11-18 14:09:23 +0000

    vite config removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/bi', 'react-icons/ai', 'react-icons/fi', 'react-icons/md', 'react-icons/ri', 'react-icons/bs', 'react-icons/gi', 'react-icons/fa', 'react-icons/io', 'react-icons/im', 'react-icons/io5', 'react-icons/si', 'react-icons/ti', 'react-icons/vsc', 'react-icons/wi', 'react-icons/go', 'react-icons/g'],
-    },
-  },
+  plugins: [react()]
 })
