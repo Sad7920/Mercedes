f392071b782281b222d3c0692050b88b936c97d3
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2023-11-18 13:57:57 +0000

    build

--- client/src/components/AddSession.jsx
+++ client/src/components/AddSession.jsx
@@ -23,7 +23,7 @@ const AddSession = ({ addSession }) => {
                 onChange={handleInputChange}
                 className='relative block w-full px-4 py-3 bg-black border-[0.2px] rounded-none appearance-none placeholder-neutral-500 text-neutral-300 col-span-4 border-neutral-700 focus:outline-none focus:z-10 sm:text-sm'
             />
-            <button className='col-span-1 w-full px-4 py-2 font-thin text-black bg-white border border-transparent rounded-sm text-sm focus:outline-none hover:border-b-4 border-b-4 border-white hover:border-b-[#00f5d0] hover:bg-neutral-200' onClick={handleAddSession}>Add</button>
+            <button className='col-span-1 w-full px-4 py-2 font-thin text-black bg-white border border-transparent rounded-sm text-sm focus:outline-none hover:border-b-3 border-b-4 border-white hover:border-b-[#00f5d0] hover:bg-neutral-200' onClick={handleAddSession}>Add</button>
         </div>
     );
 };
